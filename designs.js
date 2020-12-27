a6355b3a091231f31704af4055b156c5e04d2592
tlmknow <49690640+tlmknow@users.noreply.github.com>
2020-12-27 16:38:22 -0600

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,13 +3,12 @@ const pickColor = document.getElementById("colorPicker");
 // Select size input
 const table = document.getElementById("pixelCanvas");
 const sizePicker = document.getElementById("sizePicker");
+const height = document.getElementById("inputHeight")
+const width = document.getElementById("inputWidth")
+
 
 sizePicker.addEventListener("submit", function() {
 	event.preventDefault();
-
-
-	height = document.getElementById("inputHeight").value;
-	width = document.getElementById("inputWidth").value;
 	table.innerHTML = null;
 	makeGrid(height, width);
 });
@@ -17,16 +16,16 @@ sizePicker.addEventListener("submit", function() {
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid(height, width) {
+function makeGrid() {
 
-//create row and columns
-	for (r = 0; r <= height; r++) {
+//Creates rows and cells
+	for (r = 0; r <= height.value; r++) {
 		let row = table.insertRow();
-		for (c = 0; c <= width; c++) {
-			let cell = row.insertCell()
-			cell.addEventListener("click", function() {
-//sets the background color for the selected cell
-				cell.style.backgroundColor = pickColor.value;
+		for (c = 0; c <= width.value; c++) {
+			let column = row.insertCell()
+			//sets the background color for the selected cell
+			column.addEventListener("click", function() {
+      column.style.backgroundColor = pickColor.value;
 			})
 		}
 
